0109b0b676fbd5c5fc4436d21220649e98a99be5
Bassel Al Araaj <info@basselalaraaj.com>
2020-04-06 23:17:12 +0200

    adding destination header on the destination screen

--- lib/screens/destination_screen.dart
+++ lib/screens/destination_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travel/models/destination_model.dart';
 
 class DestinationScreen extends StatefulWidget {
@@ -11,7 +12,92 @@ class DestinationScreen extends StatefulWidget {
 class _DestinationScreenState extends State<DestinationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('${widget.destionation.city}')),
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(30.0)),
+                height: MediaQuery.of(context).size.width,
+                child: Hero(
+                  tag: widget.destionation.imageUrl,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: Image(
+                      image: AssetImage(widget.destionation.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0, vertical: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      iconSize: 30.0,
+                      color: Colors.black,
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.search),
+                          iconSize: 30.0,
+                          color: Colors.black,
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.menu),
+                          iconSize: 30.0,
+                          color: Colors.black,
+                          onPressed: () => Navigator.pop(context),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 20.0,
+                left: 20.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      '${widget.destionation.city}',
+                      style: TextStyle(
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.locationArrow,
+                          size: 15.0,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          '${widget.destionation.country}',
+                          style: TextStyle(fontSize: 20.0, color: Colors.white),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
--- lib/widgets/destination_carousel.dart
+++ lib/widgets/destination_carousel.dart
@@ -101,13 +101,16 @@ class DestinationsCarousel extends StatelessWidget {
                               ]),
                           child: Stack(
                             children: <Widget>[
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(20.0),
-                                child: Image(
-                                  height: 180.0,
-                                  width: 180.0,
-                                  image: AssetImage(destination.imageUrl),
-                                  fit: BoxFit.cover,
+                              Hero(
+                                tag: destination.imageUrl,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  child: Image(
+                                    height: 180.0,
+                                    width: 180.0,
+                                    image: AssetImage(destination.imageUrl),
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
                               Positioned(
