034204b4a26c2d4b54bc1979479444f40aefcd9c
Bassel Al Araaj <info@basselalaraaj.com>
2020-04-08 21:38:56 +0200

    adding activity list view

--- lib/screens/destination_screen.dart
+++ lib/screens/destination_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travel/models/activity_model.dart';
 import 'package:travel/models/destination_model.dart';
 
 class DestinationScreen extends StatefulWidget {
@@ -95,6 +96,88 @@ class _DestinationScreenState extends State<DestinationScreen> {
                 ),
               ),
             ],
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: widget.destionation.activities.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Activity activity = widget.destionation.activities[index];
+                  return Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                      height: 170.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image(
+                                image: AssetImage(activity.imageUrl),
+                                height: 150.0,
+                                width: 150.0,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Container(
+                              width: 200.0,
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 10.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Container(
+                                            width: 120.0,
+                                            child: Text(
+                                              "${activity.name}",
+                                              style: TextStyle(
+                                                  fontSize: 18.0,
+                                                  fontWeight: FontWeight.w600),
+                                            )),
+                                        Column(
+                                          children: <Widget>[
+                                            Text(
+                                              "\$${activity.price}",
+                                              style: TextStyle(
+                                                  fontSize: 22.0,
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                            Text(
+                                              "per pax",
+                                              style: TextStyle(
+                                                fontSize: 12.0,
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                      "${activity.type}",
+                                      style: TextStyle(
+                                        fontSize: 12.0,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ));
+                }),
           )
         ],
       ),
